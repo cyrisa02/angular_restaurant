fc9e58d6f0edb0e584e9d819453670f846883444
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-11-21 10:58:21 +0100

    Add Json-server and component dashboard and service restaurant

---
+++ readme.txt
@@ -0,0 +1,9 @@
+1/ Installation d'une Navbar avec Material
+2/ Installation de JSON -Server 
+https://www.npmjs.com/package/json-server
+création d'un fichier db.json
+3/ Créer un component dashboard-> ng g c dashboard
+4/ Routing
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
